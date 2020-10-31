15ff0ccc68bd02f34f5a3759fc93c721bf75bfe8
Nick Reshetnikov <nick.reshetnikov+1@gmail.com>
2020-10-31 15:00:40 -0700

    simple solidity contract
    - allows for a contract to be initialized with a list of strings
    - allows for some one to vote for one of the strings in the list
    - allows  for some one to get total votes received by one of the strings

---
+++ rating/contracts/Rating.sol
@@ -0,0 +1,19 @@
+pragma solidity >=0.4.22 <0.8.0;
+
+contract Rating{
+  mapping (bytes32 => uint8) public ratingsReceived;
+
+  bytes32[] public bandList;
+
+  function Rating(bytes32[] bandNames) public {
+    bandList = bandNames;
+  }
+
+  function totalVotesFor(bytes32 band) view public returns (uint8) {
+    return ratingsReceived[band];
+  }
+
+  function voteForBand(bytes32 band) public {
+    ratingsReceived[band] += 1;
+  }
+}
