8d9a1f326aaf49cd3406654f2d406d9309c148e3
Nick Reshetnikov <nick.reshetnikov@gmail.com>
2022-05-22 12:04:51 -0700

    DRY
    - define band names only once and refer to variables, rather than hard-coding in all the assertions

--- rating/test/TestRating.sol
+++ rating/test/TestRating.sol
@@ -6,25 +6,26 @@ import "../contracts/Rating.sol";
 
 contract TestRating {
   Rating ratingContract = Rating(DeployedAddresses.Rating());
+
+  string expectedFirstBand = "Children of Bodom";
+  string expectedSecondBand = "Pentagram";
   uint8 indexForVoteOf10 = 9;
 
   function testInitialBandListInDeployedContract() public {
-    string memory expectedFirstBand = "Children of Bodom";
-    string memory expectedSecondBand = "Pentagram";
+
     uint expectedNumVotes = 0;
 
-    Assert.equal(ratingContract.bandList(0), expectedFirstBand, "The first band should be Children of Bodom");
-    Assert.equal(ratingContract.bandList(1), expectedSecondBand, "The second band should be Pentagram");
-    Assert.equal(ratingContract.numVotes("Children of Bodom", indexForVoteOf10), expectedNumVotes, "There should be 0 votes of 10 for Children of Bodom");
-    Assert.equal(ratingContract.numVotes("Pentagram", indexForVoteOf10), expectedNumVotes, "There should be 0 votes of 10 for Pentagram");
+    Assert.equal(ratingContract.bandList(0), expectedFirstBand, string(abi.encodePacked("The first band should be ", expectedFirstBand)));
+    Assert.equal(ratingContract.bandList(1), expectedSecondBand, string(abi.encodePacked("The second band should be ", expectedSecondBand)));
+    Assert.equal(ratingContract.numVotes(expectedFirstBand, indexForVoteOf10), expectedNumVotes, string(abi.encodePacked("There should be 0 votes of 10 for ", expectedFirstBand)));
+    Assert.equal(ratingContract.numVotes(expectedFirstBand, indexForVoteOf10), expectedNumVotes, string(abi.encodePacked("There should be 0 votes of 10 for ", expectedSecondBand)));
   }
 
   function testVotingForBand() public {
-    string memory expectedFirstBand = "Children of Bodom";
     uint expectedNumVotes = 1;
 
-    ratingContract.voteForBand("Children of Bodom", 10);
+    ratingContract.voteForBand(expectedFirstBand, 10);
 
-    Assert.equal(ratingContract.numVotes("Children of Bodom", indexForVoteOf10), expectedNumVotes, "There should be 1 vote of 10 for Children of Bodom");
+    Assert.equal(ratingContract.numVotes(expectedFirstBand, indexForVoteOf10), expectedNumVotes, string(abi.encodePacked("There should be 1 vote of 10 for ", expectedFirstBand)));
   }
 }
